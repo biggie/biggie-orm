87850fd8ee73d1f9def5e94fed9f368b0573427e
Tim-Smart <tim@fostle.com>
2010-09-23 02:23:36 +1200

    Persitance and passing tests

---
+++ Makefile
@@ -0,0 +1,4 @@
+test:
+	@expresso -I lib --serial test/*.test.js
+
+.PHONY: test
--- test/model.test.js
+++ test/model.test.js
@@ -1,5 +1,8 @@
 var orm = require('orm');
 
+orm.pending || (orm.pending = 0);
+++orm.pending;
+
 var User = orm.model('User', {
   name: {type: 'string'},
   email: {type: 'string'},
@@ -10,6 +13,9 @@ var User = orm.model('User', {
 });
 
 module.exports = {
+  setup: function (callback) {
+    User.clear(callback);
+  },
   'Can create model': function (assert) {
     var user = new User();
 
@@ -28,5 +34,59 @@ module.exports = {
     });
 
     assert.ok(user.getName() === 'Tim');
+  },
+  'Can save model': function (assert, done) {
+    var user = new User({
+      name: 'Tim'
+    });
+
+    assert.ok(user.isNew);
+    assert.ok(!user.id);
+    assert.ok(!user.hasErrors);
+
+    user.save(function (error, model) {
+      assert.ok(!error);
+      assert.equal(user, model);
+      assert.ok(!user.isNew);
+      assert.ok(user.id);
+      assert.equal(user.name, 'Tim');
+      done();
+    });
+  },
+  'Can remove unsaved model': function (assert, done) {
+    var user = new User({
+      name: 'Tim'
+    });
+
+    user.remove(function (error, model) {
+      assert.ok(!error);
+      assert.equal(user, model);
+      assert.ok(user.isNew);
+      assert.ok(!user.isRemoved);
+      done();
+    });
+  },
+  'Can remove model': function (assert, done) {
+    var user = new User({
+      name: 'Tim'
+    });
+
+    assert.ok(user.isNew);
+    assert.ok(!user.id);
+    assert.ok(!user.hasErrors);
+
+    user.save(function (error, model) {
+      assert.ok(!error);
+      user.remove(function (error, model) {
+        assert.ok(!error);
+        assert.equal(model.isRemoved, true);
+        assert.ok(model.isNew);
+        // TODO: Add test to check if still exists
+        done();
+      });
+    });
+  },
+  after: function () {
+    --orm.pending || orm.db.end();
   }
 };
